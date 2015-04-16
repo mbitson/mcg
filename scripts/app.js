a8989cbca27466183917684dcc169c28eb22a360
mbitson <importune@gmail.com>
2015-04-16 13:01:06 -0400

    Added tons!

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,73 @@
 "use strict";
 
-var mcgApp = angular.module('mcgApp', ['ngRoute', 'ngMaterial', 'ngAnimate']);
-
-mcgApp.config(function ($routeProvider) {
+var mcgApp = angular.module('mcgApp', ['ngRoute', 'ngMaterial', 'ngAnimate', 'angularSpectrumColorpicker', 'ngMdIcons']);
+mcgApp.config(function ($routeProvider, $mdThemingProvider) {
    	$routeProvider.when('/', {
         templateUrl: 'templates/color_generator.html',
         controller: 'ColorGeneratorCtrl'
-    })
+    });
+    $mdThemingProvider.definePalette('darkest', {
+        "50": "#e6e6e6",
+        "100": "#b3b3b3",
+        "200": "#808080",
+        "300": "#555555",
+        "400": "#2a2a2a",
+        "500": "#000000",
+        "600": "#000000",
+        "700": "#000000",
+        "800": "#000000",
+        "900": "#000000",
+        "A100": "#555555",
+        "A200": "#555555",
+        "A400": "#555555",
+        "A700": "#555555"
+    });
+    $mdThemingProvider.definePalette('dark', {
+        "50": "#e9e9e9",
+        "100": "#bebebe",
+        "200": "#939393",
+        "300": "#6e6e6e",
+        "400": "#4a4a4a",
+        "500": "#262626",
+        "600": "#212121",
+        "700": "#1d1d1d",
+        "800": "#181818",
+        "900": "#131313",
+        "A100": "#6e6e6e",
+        "A200": "#6e6e6e",
+        "A400": "#6e6e6e",
+        "A700": "#6e6e6e"
+    });
+    $mdThemingProvider.theme('default')
+        .primaryPalette('darkest')
+        .accentPalette('dark')
+        .dark();
 });
 
+/*
+ * Color utility functions
+ * Source: http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+ */
+function shadeColor(color, percent) {
+    var f = parseInt(color.slice(1), 16), t = percent < 0 ? 0 : 255, p = percent < 0 ? percent * -1 : percent, R = f >> 16, G = f >> 8 & 0x00FF, B = f & 0x0000FF;
+    return "#" + (0x1000000 + (Math.round((t - R) * p) + R) * 0x10000 + (Math.round((t - G) * p) + G) * 0x100 + (Math.round((t - B) * p) + B)).toString(16).slice(1);
+}
+mcgApp.filter('darken', function(){
+    return function(color){
+        return shadeColor(color,-0.1);
+    };
+});
+mcgApp.filter('lighten', function(){
+    return function(color){
+        return shadeColor(color,0.1);
+    };
+});
+
+// Utility functions
+// TODO: Refactor into some sort of helper
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
+};
--- scripts/controllers/ColorGeneratorCtrl.js
+++ scripts/controllers/ColorGeneratorCtrl.js
@@ -1,20 +1,63 @@
 "use strict";
 
 mcgApp.controller('ColorGeneratorCtrl',
-function ($scope) {
-	$scope.mColors = [];
-	$scope.baseColor = "#26a69a";
-	$scope.mColorsExportable = [];
-	$scope.$watch('baseColor', function(newValue, oldValue){
-		if($scope.rgbColor !== null){
-			$scope.mColorsCalculated = $scope.computeColors($scope.baseColor);
-			$scope.mColors = $scope.mColorsCalculated;
-			$scope.mColorsExportable = $scope.makeExportable($scope.mColors);
+function ($scope, $mdDialog)
+{
+	$scope.init = function ()
+	{
+		// Define base palette
+		$scope.palette = {
+			colors: [],
+			orig  : [],
+			base  : '#26a69a',
+			json  : '',
+			name  : ''
+		};
+
+		// Define palettes
+		$scope.palettes = [];
+
+		// Add base palette
+		$scope.addBasePalette();
+
+		// Define theme defaults
+		$scope.theme = {
+			name: '',
+            palettes: $scope.palettes
+		};
+	};
+
+	// Function to add a palette to palettes.
+	$scope.addBasePalette = function(){
+		$scope.palettes.push(angular.copy($scope.palette));
+		$scope.calcPalette($scope.palettes.length-1);
+	};
+
+	// Function to calculate all colors for all palettes
+	$scope.calcPalettes = function(){
+		for(var i = 0; i < $scope.palettes.length; i++){
+			$scope.calcPalette(i);
 		}
-	});
+	};
+
+	// Function to delete a palette when passed it's key.
+	$scope.deletePalette = function(key){
+		$scope.palettes.remove(key);
+	};
+
+	// Function to assign watchers to all bases
+	$scope.calcPalette = function(key){
+		$scope.palettes[key].orig = $scope.computeColors($scope.palettes[key].base);
+		$scope.palettes[key].colors = $scope.palettes[key].orig;
+	};
+
+	// Function to make the definePalette code for a palette.
+	$scope.createDefinePalette = function(palette){
+		return '$mdThemingProvider.definePalette(\'' + palette.name + '\', ' + $scope.makeColorsJson(palette.colors) + ');';
+	};
 
 	// Function to make an exportable json array for themes.
-	$scope.makeExportable = function(colors){
+	$scope.makeColorsJson = function(colors){
 		var exportable = {};
 		angular.forEach(colors, function(value, key){
 			exportable[value.name] = value.hex;
@@ -40,11 +83,11 @@ function ($scope) {
 				name : '200'
 			},
 			{
-				hex: shadeColor(color, 0.25),
+				hex: shadeColor(color, 0.333),
 				name : '300'
 			},
 			{
-				hex: shadeColor(color, 0.125),
+				hex: shadeColor(color, 0.166),
 				name : '400'
 			},
 			{
@@ -66,22 +109,96 @@ function ($scope) {
 			{
 				hex : shadeColor(color, -0.5),
 				name: '900'
+			},
+			{
+				hex : shadeColor(color, 0.7),
+				name: 'A100'
+			},
+			{
+				hex : shadeColor(color, 0.5),
+				name: 'A200'
+			},
+			{
+				hex : shadeColor(color, 0.166),
+				name: 'A400'
+			},
+			{
+				hex : shadeColor(color, -0.25),
+				name: 'A700'
 			}
 		];
 	};
 
-	/*
-	 * Color utility functions
-	 * Source: http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
-	 */
-	function shadeColor(color, percent) {
-		var f = parseInt(color.slice(1), 16), t = percent < 0 ? 0 : 255, p = percent < 0 ? percent * -1 : percent, R = f >> 16, G = f >> 8 & 0x00FF, B = f & 0x0000FF;
-		return "#" + (0x1000000 + (Math.round((t - R) * p) + R) * 0x10000 + (Math.round((t - G) * p) + G) * 0x100 + (Math.round((t - B) * p) + B)).toString(16).slice(1);
-	}
-	function darken(color){
-		shadeColor(color,-0.1);
-	}
-	function lighten(color) {
-		shadeColor(color,0.1);
-	}
+    // Function to show theme's full code
+    $scope.showThemeCode = function()
+    {
+        // Init return string
+        var themeCodeString = '';
+
+        // For each palette, add it's declaration
+        for(var i = 0; i < $scope.palettes.length; i++){
+            themeCodeString = themeCodeString+$scope.createDefinePalette($scope.palettes[i])+'\n\r';
+        }
+
+        // Add theme configuration
+        themeCodeString = themeCodeString +
+        '$mdThemingProvider.theme(\'' + $scope.theme.name + '\')\n\r\t'+
+        '.primaryPalette(\''+$scope.palettes[0].name+'\')\n\r\t'+
+        '.accentPalette(\''+$scope.palettes[1].name+'\');'
+        +'\n\r';
+
+        // Show clipboard with theme code
+        $scope.showClipboard(themeCodeString);
+    };
+
+	// Function to regenerate json and show dialog for palette.
+	$scope.showPaletteCode = function(palette){
+		palette.json = $scope.createDefinePalette(palette);
+		$scope.showClipboard(palette.json);
+	};
+
+    // Function to show export json for loading carts later
+    $scope.showExport = function(){
+        $scope.showClipboard(JSON.stringify($scope.theme, null, 2));
+    };
+
+	// Function to show generic clipboard alert dialog
+	$scope.showClipboard = function(code){
+		$mdDialog.show({
+			template   : '<md-dialog aria-label="Clipboard dialog">' +
+			'  <md-content>' +
+			'    <pre>{{code}}' +
+			'    </pre>' +
+			'  </md-content>' +
+			'  <div class="md-actions">' +
+			'    <md-button ng-click="closeDialog()">' +
+			'      Close' +
+			'    </md-button>' +
+			'  </div>' +
+			'</md-dialog>',
+			locals     : {
+				code: code
+			},
+			controller : ClipboardDialogController
+		});
+		function ClipboardDialogController(scope, $mdDialog, code) {
+			scope.code = code;
+			scope.closeDialog = function () {
+				$mdDialog.hide();
+			}
+		}
+	};
+
+    // Function to darken a palette's color.
+    $scope.darkenPaletteItem = function(color){
+        color.hex = shadeColor(color.hex, -0.1);
+    };
+
+    // Function to lighten a palette's color.
+    $scope.lightenPaletteItem = function(color){
+        color.hex = shadeColor(color.hex, 0.1);
+    };
+
+	// Init controller
+	$scope.init();
 });
