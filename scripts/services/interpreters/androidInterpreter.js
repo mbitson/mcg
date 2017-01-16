755a4ee3a7121a5c7295f56d3ae3c6b6baa5b4e0
mbitson <importune@gmail.com>
2017-01-16 13:47:27 -0500

    Added interpreters and cleaned up export dialog controller.

---
+++ scripts/services/interpreters/androidInterpreter.js
@@ -0,0 +1,40 @@
+mcgApp.service('AndroidInterpreter', function () {
+    this.export = function(exportObj, theme)
+    {
+        this.code = '';
+        this.exportObj = exportObj;
+        this.theme = theme;
+        this.buildExport();
+        return this.code;
+    };
+
+    /*
+     * Android formatting functions
+     */
+    this.buildExport = function () {
+        if (this.single === true) {
+            // Generate palette's code
+            themeCodeString = this.createAndroidPaletteCode(this.exportObj);
+        } else {
+            // Init return string
+            var themeCodeString = '';
+
+            // For each palette, add it's declaration
+            for (var i = 0; i < this.exportObj.length; i++) {
+                themeCodeString = themeCodeString + this.createAndroidPaletteCode(this.exportObj[i]);
+            }
+        }
+
+        // Add XML parent node
+        this.code = escapeHtml('<resources>\n' + themeCodeString + '<resources>');
+    };
+
+    this.createAndroidPaletteCode = function (palette) {
+        var code = '';
+        angular.forEach(palette.colors, function (value, key) {
+            code += '     <color name="' + palette.name + '_' + value.name + '">' + value.hex + '</color>\n';
+        });
+        return code;
+    };
+
+});
---
+++ scripts/services/interpreters/angularJs2Interpreter.js
@@ -0,0 +1,56 @@
+mcgApp.service('AngularJs2Interpreter', function () {
+    this.export = function(exportObj, theme)
+    {
+        this.code = '';
+        this.exportObj = exportObj;
+        this.theme = theme;
+        this.buildExport();
+        return this.code;
+    };
+
+    /*
+     * AngularJS 2
+     * Material 2 Formatting Functions
+     */
+    this.buildExport = function () {
+        var themeCodeString = '/* For use in src/lib/core/theming/_palette.scss */\n';
+        if (this.single === true) {
+            // Generate palette's code
+            themeCodeString = this.createMTwoPaletteCode(this.exportObj);
+        } else {
+            // For each palette, add it's declaration
+            for (var i = 0; i < this.exportObj.length; i++) {
+                themeCodeString = themeCodeString + this.createMTwoPaletteCode(this.exportObj[i]);
+            }
+        }
+
+        this.code = themeCodeString;
+    };
+
+    this.createMTwoPaletteCode = function (palette) {
+        var code = '';
+
+        // Generate base colors
+        code += '$md-' + palette.name + ': (\n';
+        angular.forEach(palette.colors, function (value, key) {
+            code += "    " + value.name + " : " + tinycolor(value.hex).toHexString() + ',\n';
+        });
+
+        // Generate the contrast variables
+        code += '    contrast: (\n';
+        angular.forEach(palette.colors, function (value, key) {
+            if (value.darkContrast) {
+                var contrast = '#000000';
+            } else {
+                var contrast = '#ffffff';
+            }
+            code += "        " + value.name + " : " + contrast + ',\n';
+        });
+        code += '    )\n';
+
+        code += ');\n\n';
+
+        return code;
+    };
+
+});
