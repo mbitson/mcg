2271936bcf791472003b5bef9c17ab179fa54c6e
mbitson <importune@gmail.com>
2020-05-01 01:47:33 -0400

    Adding in support for AngularJS 2 (material) code to be imported to MCG.

--- scripts/services/interpreters/angularJs2Interpreter.js
+++ scripts/services/interpreters/angularJs2Interpreter.js
@@ -1,57 +1,104 @@
 mcgApp.service('AngularJs2Interpreter', function () {
-    this.export = function(exportObj, theme, single)
-    {
-        this.code = '';
-        this.exportObj = exportObj;
-        this.theme = theme;
-        this.single = single;
-        this.buildExport();
-        return this.code;
-    };
 
-    /*
-     * AngularJS 2
-     * Material 2 Formatting Functions
+    /**
+     * Generates a string of AngularJS 2 code from the MCG Palettes
+     * @param exportObj
+     * @param theme
+     * @param single
+     * @returns {string}
      */
-    this.buildExport = function () {
-        var themeCodeString = '/* For use in src/lib/core/theming/_palette.scss */\n';
-        if (this.single === true) {
-            // Generate palette's code
-            themeCodeString = this.createMTwoPaletteCode(this.exportObj);
+    this.export = function (exportObj, theme, single) {
+        let themeCodeString = '/* For use in src/lib/core/theming/_palette.scss */\n';
+        if (single === true) {
+            themeCodeString += this.createMTwoPaletteCode(exportObj);
         } else {
-            // For each palette, add it's declaration
-            for (var i = 0; i < this.exportObj.length; i++) {
-                themeCodeString = themeCodeString + this.createMTwoPaletteCode(this.exportObj[i]);
+            for (let i = 0; i < exportObj.length; i++) {
+                themeCodeString += this.createMTwoPaletteCode(exportObj[i]);
             }
         }
-
-        this.code = themeCodeString;
+        return themeCodeString;
     };
 
-    this.createMTwoPaletteCode = function (palette) {
-        var code = '';
+    /**
+     * Attempts to generate a Palettes object that MCG will recognize from a string of AngularJS 2 code.
+     * @param code
+     * @returns {[]}
+     */
+    this.import = function (code) {
+        let palettes = [];
+        for (const paletteData of [...code.matchAll(/\$md-(.*): ?([\n:a-zA-z0-9 ('#,)]*)\);/g)]) {
+            let palette = this.buildPalette(paletteData);
+            palettes.push(palette);
+        }
+        return palettes;
+    }
 
-        // Generate base colors
+    /**
+     * Checks for the presence of a string that is only found in this interpreter.
+     * @param code string
+     * @returns {boolean}
+     */
+    this.isApplicable = function (code) {
+        // Checks for line: `$md-<PALETTENAME>: (`
+        // Checks for line: `contrast: (`
+        return !!(code.match(/\$md-(.*): ?\(/g) && code.match(/contrast: ?\(/g));
+    };
+
+    /**
+     * Creates the android XML for a particular palette
+     * @param palette
+     * @returns {string}
+     */
+    this.createMTwoPaletteCode = function (palette) {
+        let code = '';
         code += '$md-' + palette.name + ': (\n';
-        angular.forEach(palette.colors, function (value, key) {
+        angular.forEach(palette.colors, function (value) {
             code += "    " + value.name + " : " + tinycolor(value.hex).toHexString() + ',\n';
         });
-
-        // Generate the contrast variables
         code += '    contrast: (\n';
-        angular.forEach(palette.colors, function (value, key) {
-            if (value.darkContrast) {
-                var contrast = '#000000';
-            } else {
-                var contrast = '#ffffff';
-            }
+        angular.forEach(palette.colors, function (value) {
+            let contrast = '#ffffff';
+            if (value.darkContrast) contrast = '#000000';
             code += "        " + value.name + " : " + contrast + ',\n';
         });
         code += '    )\n';
-
         code += ');\n\n';
-
         return code;
     };
+
+    /**
+     * Builds a palette object for MCG from regex-extracted data
+     * @param paletteData
+     * @returns {{orig: [], name: *, json: string, colors: [], base: string}}
+     */
+    this.buildPalette = function (paletteData) {
+        let palette = {"colors": [], "orig": [], "base": "", "json": "", "name": paletteData[1], "tmpColors": {}}
+
+        for (const color of [...paletteData[2].matchAll(/\n[\t ]*(.*) ?: ?#(.*),/g)]) {
+            let colorName = color[1];
+            let colorHex = color[2];
+            if (!(colorName in palette.tmpColors)) {
+                palette.tmpColors[colorName] = {
+                    "name": colorName,
+                    "hex": '#' + colorHex,
+                    "darkContrast": true
+                };
+            } else {
+                if (colorHex.toLowerCase() === 'ffffff') {
+                    palette.tmpColors[colorName].darkContrast = false;
+                }
+            }
+            if (colorName === "500" && (colorHex.toLowerCase() !== 'ffffff' || colorHex !== '000000')) {
+                palette.base = '#' + colorHex;
+            }
+        }
+
+        angular.forEach(palette.tmpColors, function (color) {
+            palette.colors.push(color);
+            palette.orig.push(color);
+        });
+        delete (palette.tmpColors);
+        return palette;
+    }
 
 });
