8e46d06c75e310fefb4825c189953ea5792e1f4c
mbitson <importune@gmail.com>
2020-05-01 01:46:50 -0400

    Adding in support for Android XML code to be imported to MCG.

--- scripts/services/interpreters/androidInterpreter.js
+++ scripts/services/interpreters/androidInterpreter.js
@@ -1,41 +1,104 @@
 mcgApp.service('AndroidInterpreter', function () {
-    this.export = function(exportObj, theme, single)
-    {
-        this.code = '';
-        this.exportObj = exportObj;
-        this.theme = theme;
-        this.single = single;
-        this.buildExport();
-        return this.code;
-    };
 
-    /*
-     * Android formatting functions
+    /**
+     * Generates a string of Android code from the MCG Palettes
+     * @param exportObj
+     * @param theme
+     * @param single
+     * @returns {string}
      */
-    this.buildExport = function () {
-        if (this.single === true) {
-            // Generate palette's code
-            themeCodeString = this.createAndroidPaletteCode(this.exportObj);
+    this.export = function (exportObj, theme, single) {
+        let themeCodeString = '';
+        if (single === true) {
+            themeCodeString += this.createAndroidPaletteCode(exportObj);
         } else {
-            // Init return string
-            var themeCodeString = '';
-
-            // For each palette, add it's declaration
-            for (var i = 0; i < this.exportObj.length; i++) {
-                themeCodeString = themeCodeString + this.createAndroidPaletteCode(this.exportObj[i]);
+            for (let i = 0; i < exportObj.length; i++) {
+                themeCodeString += this.createAndroidPaletteCode(exportObj[i]);
             }
         }
-
-        // Add XML parent node
-        this.code = escapeHtml('<resources>\n' + themeCodeString + '<resources>');
+        return escapeHtml('<resources>\n' + themeCodeString + '<resources>');
     };
 
+    /**
+     * Attempts to generate a Palettes object that MCG will recognize from a string of Android code.
+     * @param code
+     * @returns {[]}
+     */
+    this.import = function (code) {
+        let colors = this.determineColors(code);
+        return this.determinePalettes(colors);
+    }
+
+    /**
+     * Checks for the presence of a string that is only found in this interpreter.
+     * @param code string
+     * @returns {boolean}
+     */
+    this.isApplicable = function (code) {
+        // Checks for line: `<resources>`
+        // Checks for line: `<color name="<PALETTECOLORNAME>">#<PALETTECOLORHEX></color>
+        return !!(code.match(/<resources>/g) && code.match(/<color name="(.*)">#[a-zA-Z0-9]{3,6}<\/color>/g));
+    };
+
+    /**
+     * Creates the android XML for a particular palette
+     * @param palette
+     * @returns {string}
+     */
     this.createAndroidPaletteCode = function (palette) {
-        var code = '';
-        angular.forEach(palette.colors, function (value, key) {
+        let code = '';
+        angular.forEach(palette.colors, function (value) {
             code += '     <color name="' + palette.name + '_' + value.name + '">' + value.hex + '</color>\n';
         });
         return code;
     };
+
+    /**
+     * Takes AndroidXML and determines the palettes, color names, and color values.
+     * @param code
+     * @returns {{}}
+     */
+    this.determineColors = function (code) {
+        let colors = {};
+        for (const line of [...code.matchAll(/<color name="(.*)">#([a-zA-Z0-9]{3,6})<\/color>/g)]) {
+            let paletteNamePieces = line[1].split('_');
+            let paletteName = paletteNamePieces[0];
+            let paletteColorKey = paletteNamePieces[1];
+            let paletteColor = line[2];
+
+            if (typeof colors[paletteName] === "undefined") {
+                colors[paletteName] = {};
+            }
+            colors[paletteName][paletteColorKey] = paletteColor;
+        }
+        return colors;
+    }
+
+    /**
+     * Generates the necessary MCG Palette objects from the palettes, color names, and color values.
+     * @param colors {{}}
+     * @returns {[]}
+     */
+    this.determinePalettes = function (colors) {
+        let palettes = [];
+        angular.forEach(colors, function (paletteColors, paletteName) {
+            let palette = {"colors": [], "orig": [], "base": "", "json": "", "name": paletteName}
+            angular.forEach(paletteColors, function (colorValue, colorName) {
+                let color = {
+                    "name": colorName,
+                    "hex": '#' + colorValue,
+                    "darkContrast": true
+                };
+                palette.colors.push(color);
+                palette.orig.push(color);
+
+                if (colorName === "500") {
+                    palette.base = '#' + colorValue;
+                }
+            });
+            palettes.push(palette);
+        });
+        return palettes;
+    }
 
 });
